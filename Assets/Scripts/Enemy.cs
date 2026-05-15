9e181945fb9de7df6a24c52d71469724e1592313
theazd <v0id.staree@gmail.com>
2026-05-15 10:38:08 +0200

    Player Health

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+   [SerializeField] private float damage;
+
+   private void OnTriggerEnter2D(Collider2D collision)
+   {
+      if (collision.tag == "Player")
+      {
+          collision.GetComponent<Health>().TakeDamage(damage);
+   }
+}
---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+        
+        if (currentHealth > 0)
+        {
+            //player hurt
+            
+        }
+        else
+        {
+            // player dead
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(1);
+        }
+    }
+}
